a8bd5ee7d431953133f420fe2e526dc65bd139a1
drrh12 <flavioandrade0201@gmail.com>
2023-08-05 20:38:58 +0900

    get information from pool

--- uniswapv3/client/Pool.js
+++ uniswapv3/client/Pool.js
@@ -1,61 +1,31 @@
 const ethers = require("ethers");
-const { CurrentConfig } = require("../config");
-const { computePoolAddress } = require("@uniswap/v3-sdk");
-const Quoter = require("@uniswap/v3-periphery/artifacts/contracts/lens/Quoter.sol/Quoter.json");
-const IUniswapV3PoolABI = require("@uniswap/v3-core/artifacts/contracts/interfaces/IUniswapV3Pool.sol/IUniswapV3Pool.json");
-const {
-  POOL_FACTORY_CONTRACT_ADDRESS,
-  QUOTER_CONTRACT_ADDRESS,
-} = require("../libs/constants");
-const { getProvider } = require("../libs/providers");
-const { toReadableAmount, fromReadableAmount } = require("../libs/conversion");
 
-async function quote() {
-  const quoterContract = new ethers.Contract(
-    QUOTER_CONTRACT_ADDRESS,
-    Quoter.abi,
-    getProvider()
-  );
+// The JSON RPC provider
+const provider = new ethers.providers.JsonRpcProvider(
+  "https://mainnet.infura.io/v3/d341f6b6b99a4c66be728d753b3e8534"
+);
 
-  const poolConstants = await getPoolConstants();
+// Address of the Uniswap V3 pool
+const poolAddress = "0x5aB53EE1d50eeF2C1DD3d5402789cd27bB52c1bB";
 
-  const quotedAmountOut = await quoterContract.callStatic.quoteExactInputSingle(
-    poolConstants.token0,
-    poolConstants.token1,
-    poolConstants.fee,
-    fromReadableAmount(
-      CurrentConfig.tokens.amountIn,
-      CurrentConfig.tokens.in.decimals
-    ).toString(),
-    0
-  );
+// Uniswap V3 pool ABI
+const poolABI = [
+  "function fee() external view returns (uint24)",
+  "function token0() external view returns (address)",
+  "function token1() external view returns (address)",
+  // Add more function signatures here as needed
+];
 
-  return toReadableAmount(quotedAmountOut, CurrentConfig.tokens.out.decimals);
+// Create a contract instance
+const poolContract = new ethers.Contract(poolAddress, poolABI, provider);
+
+// Usage
+async function getPoolDetails() {
+  const fee = await poolContract.fee();
+  const token0 = await poolContract.token0();
+  const token1 = await poolContract.token1();
+
+  console.log({ fee: fee.toString(), token0, token1 });
 }
 
-async function getPoolConstants() {
-  const currentPoolAddress = computePoolAddress({
-    factoryAddress: POOL_FACTORY_CONTRACT_ADDRESS,
-    tokenA: CurrentConfig.tokens.in,
-    tokenB: CurrentConfig.tokens.out,
-    fee: CurrentConfig.tokens.poolFee,
-  });
-
-  const poolContract = new ethers.Contract(
-    currentPoolAddress,
-    IUniswapV3PoolABI.abi,
-    getProvider()
-  );
-
-  const [token0, token1, fee] = await Promise.all([
-    poolContract.token0(),
-    poolContract.token1(),
-    poolContract.fee(),
-  ]);
-
-  return {
-    token0,
-    token1,
-    fee,
-  };
-}
+getPoolDetails();
