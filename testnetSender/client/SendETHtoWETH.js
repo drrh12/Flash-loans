53446e04157235653d32ff0d711363cc93779d3b
drrh12 <flavioandrade0201@gmail.com>
2023-08-05 21:31:59 +0900

    send transaction build

--- testnetSender/client/SendETHtoWETH.js
+++ testnetSender/client/SendETHtoWETH.js
@@ -2,7 +2,7 @@ const { ethers } = require("ethers");
 
 //testnet provider
 const provider = new ethers.providers.JsonRpcProvider(
-  "https://eth-sepolia.g.alchemy.com/v2/56UUW5bGJaQ-8VHNU92tdrhM7Szi2pay"
+  "https://eth-sepolia.g.alchemy.com/v2/56UUW5bGJaQ-8VHNU92tdrhM7Szi2pay" //testnet provider
 );
 
 // create signer
@@ -11,4 +11,20 @@ const privateKey =
   "a7924f44386ff1ada08d4f5769f133ab7eecf85a7cc270fc3053f874f3739d2b";
 const walletSigner = new ethers.Wallet(privateKey, provider);
 
-console.log(walletSigner);
+const exchangeETH = async () => {
+  const gasPrice = await provider.getGasPrice();
+  const nonce = 0; // web3.eth.getTransactionCount(myAddress)
+  const value = "0.05";
+  const txBuild = {
+    from: myAddress,
+    to: "0xb16F35c0Ae2912430DAc15764477E179D9B9EbEa", //Sepolia test network
+    value: ethers.utils.parseEther(value),
+    nonce: nonce,
+    gasLimit: 10000,
+    gasPrice: gasPrice,
+  };
+  //send the transaction
+  console.log(txBuild);
+};
+
+exchangeETH();
